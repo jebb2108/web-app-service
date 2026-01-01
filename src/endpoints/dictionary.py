ffc363dde0119c3bafdb93c43f92c7695ab1b145
jebb <gabouchard2002@gmail.com>
2026-01-01 18:42:26 +0300

    bug fix

--- src/endpoints/dictionary.py
+++ src/endpoints/dictionary.py
@@ -88,8 +88,8 @@ async def api_search_word_handler(
             user_word = user_word_resp.json()
             all_user_words = all_words_resp.json()
 
-            logger.info('user word: ', user_word)
-            logger.info('all words: ', all_user_words)
+            logger.info(f'user word: {user_word}')
+            logger.info(f'all words: {all_user_words}')
 
             # От лица пользователя не должно быть слова от
             # самого пользователя в словаре других пользователей, соответсвенно удаляем
